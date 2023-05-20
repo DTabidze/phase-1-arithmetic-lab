0b6bd93c20592692dd5d785e0c950dfa44dfb43c
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-20 13:38:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// function multiplied(num1,num2) {
+
+// }
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(11,20,1,3,4,13);
